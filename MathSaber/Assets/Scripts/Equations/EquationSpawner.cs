2a02c1e827070e4d89fb2fdc1753ccea9c9bc12b
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-10-23 17:37:12 -0600

    Equations (#3)
    * Added Basic Equation Solver
    
    * Basic Gameplay

---
+++ MathSaber/Assets/Scripts/Equations/EquationSpawner.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using General;
+using UnityEngine;
+using Utils;
+
+namespace Equations
+{
+    public class EquationSpawner : MonoBehaviour
+    {
+        public List<Transform> spawnPoints;
+        public float secondsBetweenEachObject;
+        public EquationManager equationManager;
+        public Transform blockHolder;
+
+        private float _currentTime;
+        private List<CubeMovementAndDestruction> _cubes;
+
+        #region Unity Functions
+
+        private void Start()
+        {
+            _currentTime = secondsBetweenEachObject;
+            _cubes = new List<CubeMovementAndDestruction>();
+        }
+
+        private void Update()
+        {
+            _currentTime -= Time.deltaTime;
+            if (_currentTime <= 0)
+            {
+                SpawnEquation();
+                _currentTime = secondsBetweenEachObject;
+            }
+        }
+
+        #endregion
+
+        #region External Functions
+
+        public void NotifyParentCollision()
+        {
+            foreach (CubeMovementAndDestruction cubeMovementAndDestruction in _cubes)
+            {
+                cubeMovementAndDestruction.SetParentCollided();
+            }
+        }
+
+        #endregion
+
+        #region Utility Functions
+
+        private void SpawnEquation()
+        {
+            _cubes.Clear();
+
+            bool correctShown = false;
+            List<Transform> copiedList = spawnPoints.ToList();
+
+            while (copiedList.Count > 0)
+            {
+                int randomIndex = Mathf.FloorToInt(Random.value * copiedList.Count);
+                Transform spawnTransform = copiedList[randomIndex];
+
+                if (!correctShown)
+                {
+                    GameObject correctGameObject = equationManager.CreateBasicEquationAndAddToUI();
+                    correctGameObject.transform.position = spawnTransform.position;
+                    correctGameObject.transform.SetParent(blockHolder);
+
+                    CubeMovementAndDestruction cubeMovement = correctGameObject.AddComponent<CubeMovementAndDestruction>();
+                    cubeMovement.SetParent(this);
+                    _cubes.Add(cubeMovement);
+
+                    correctShown = true;
+                }
+                else
+                {
+                    string answerString = equationManager.LastAnswer;
+                    int answerDigits = $"{Mathf.Abs(int.Parse(answerString))}".Length; // Very Bad. But OK for Prototype
+
+                    GameObject incorrectGameObject = equationManager.GetRandomDigitNumber(answerDigits, TagManager.InCorrectAnswer);
+                    incorrectGameObject.transform.position = spawnTransform.position;
+                    incorrectGameObject.transform.SetParent(blockHolder);
+
+                    CubeMovementAndDestruction cubeMovement = incorrectGameObject.AddComponent<CubeMovementAndDestruction>();
+                    cubeMovement.SetParent(this);
+                    _cubes.Add(cubeMovement);
+                }
+
+                copiedList.RemoveAt(randomIndex);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MathSaber/Assets/Scripts/Sword/SwordController.cs
@@ -0,0 +1,50 @@
+﻿using General;
+using TMPro;
+using UnityEngine;
+using Utils;
+
+namespace Sword
+{
+    public class SwordController : MonoBehaviour
+    {
+        private TextMeshPro _debugText;
+
+        #region Unity Functions
+
+        private void Start() => _debugText = GameObject.FindGameObjectWithTag(TagManager.DisplayText).GetComponent<TextMeshPro>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            CubeMovementAndDestruction cubeMovement = other.GetComponent<CubeMovementAndDestruction>();
+            if (!cubeMovement)
+            {
+                return;
+            }
+
+            if (cubeMovement.HasParentDetectedCollisions())
+            {
+                return;
+            }
+
+            if (other.CompareTag(TagManager.CorrectAnswer))
+            {
+                _debugText.text = "Correct Answer";
+                cubeMovement.NotifyParentCollision();
+                Destroy(other.gameObject);
+
+                Debug.Log("Correct Answer Hit");
+            }
+
+            if (other.CompareTag(TagManager.InCorrectAnswer))
+            {
+                _debugText.text = "Wrong Answer";
+                cubeMovement.NotifyParentCollision();
+                Destroy(other.gameObject);
+
+                Debug.Log("Wrong Answer Hit");
+            }
+        }
+
+        #endregion
+    }
+}
