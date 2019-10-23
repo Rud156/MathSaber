ba0a3d8b400c95381e0077d94a5c739700b1464c
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-10-23 17:31:53 -0600

    Basic Gameplay

--- MathSaber/Assets/Scripts/Equations/EquationManager.cs
+++ MathSaber/Assets/Scripts/Equations/EquationManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Utils;
 using Random = UnityEngine.Random;
 
 namespace Equations
@@ -15,24 +17,21 @@ namespace Equations
 
         [Header("Offsets")] public float singleCharOffset;
         public int wordSpacingCount;
+        public float objectScale = 1;
 
-        [Header("UI")] public TextMeshProUGUI numberDisplay;
+        [Header("Holders")] public Transform blockHolder;
+        public Transform spawnTransform;
 
-        #region Unityu Functions
+        [Header("UI")] public TextMeshPro numberDisplay;
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                SpawnBasicEquation();
-            }
-        }
+        // This is added as it is not possible to completely 
+        // Send multiple values from functions like Python
+        private string _lastEquation;
+        private string _lastAnswer;
 
-        #endregion
+        #region External Functions
 
-        #region Utility Functions
-
-        private void SpawnBasicEquation()
+        public GameObject CreateBasicEquationAndAddToUI()
         {
             int leftValue = GetRandomNumber(0, 9);
             int rightValue = GetRandomNumber(0, 9);
@@ -75,25 +74,84 @@ namespace Equations
             string questionEquation = $"{leftValue} {operatorString} {rightValue} = ";
             string answer = $"{totalValue}";
 
+            _lastEquation = questionEquation;
+            _lastAnswer = answer;
+            numberDisplay.text = questionEquation;
+
             Debug.Log($"Question: {questionEquation}, Answer: {answer}");
-            
-            float offsetLeft = answer.Length / 2.0f;
-            Vector3 startPosition = offsetLeft * singleCharOffset * Vector3.right;
+
+            return GetCombinedNumberGameObject(answer, TagManager.CorrectAnswer);
+        }
+
+        public GameObject GetRandomDigitNumber(int digitCount, string tagName)
+        {
+            int currentDigitCount = 0;
+            string randomNumberString = string.Empty;
+
+            while (currentDigitCount != digitCount)
+            {
+                var randomNumber = GetRandomNumber(currentDigitCount != 0 ? 0 : 1, 9);
+                randomNumberString += randomNumber;
+
+                currentDigitCount += 1;
+            }
+
+            return GetCombinedNumberGameObject(randomNumberString, tagName);
+        }
+
+        public string LastEquation => _lastEquation;
+
+        public string LastAnswer => _lastAnswer;
+
+        #endregion
+
+        #region Utility Functions
+
+        private GameObject GetCombinedNumberGameObject(string answer, string tagName)
+        {
+            float offsetLeft = answer.Length / 2;
+            if (answer.Length % 2 == 0)
+            {
+                // This is used for the case when the scale of the object needs to be divide by 2
+                offsetLeft -= (objectScale / 2.0f);
+            }
+
+            Vector3 startPosition = -offsetLeft * singleCharOffset * Vector3.right;
+            GameObject holderObject = new GameObject("Answer Object")
+            {
+                tag = tagName
+            };
+            Bounds holderObjectBounds = new Bounds();
 
             for (int i = 0; i < answer.Length; i++)
             {
-                string value = answer[0].ToString();
+                string value = answer[i].ToString();
                 Vector3 position = startPosition + i * singleCharOffset * Vector3.right;
 
                 if (value.Equals("-"))
                 {
                     GameObject subtractInstance = Instantiate(minusOperatorPrefab, position, Quaternion.identity);
+                    subtractInstance.transform.SetParent(holderObject.transform);
+
+                    holderObjectBounds.Encapsulate(subtractInstance.GetComponent<MeshRenderer>().bounds);
                 }
                 else
                 {
                     GameObject objectInstance = Instantiate(numbers[int.Parse(value)], position, Quaternion.identity);
+                    objectInstance.transform.SetParent(holderObject.transform);
+
+                    holderObjectBounds.Encapsulate(objectInstance.GetComponent<MeshRenderer>().bounds);
                 }
             }
+
+            BoxCollider holderCollider = holderObject.AddComponent<BoxCollider>();
+            holderCollider.size = holderObjectBounds.size;
+            holderCollider.center = Vector3.zero;
+            holderCollider.isTrigger = true;
+            
+            holderObject.transform.position = spawnTransform.position;
+
+            return holderObject;
         }
 
         private int GetRandomNumber(int start, int end) => Random.Range(start, end);
---
+++ MathSaber/Assets/Scripts/Utils/TagManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Utils
+{
+    public static class TagManager
+    {
+        public const string CorrectAnswer = "Correct";
+        public const string InCorrectAnswer = "Incorrect";
+
+        public const string DisplayText = "Display Text";
+
+        public const string Wall = "Wall";
+    }
+}
