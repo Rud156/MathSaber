5d27c93e177ccfe801cfedfa1298e57154c0afec
Namrata Doddamani <namratad7890@gmail.com>
2019-10-28 19:28:25 -0600

    audio

--- MathSaber/Assets/Scripts/Equations/EquationManager.cs
+++ MathSaber/Assets/Scripts/Equations/EquationManager.cs
@@ -32,10 +32,11 @@ namespace Equations
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                GetCombinedNumberGameObject("2", TagManager.CorrectAnswer);
-            }
+           // if (Input.GetKeyDown(KeyCode.X))
+           // {
+           //     GetCombinedNumberGameObject("2", TagManager.CorrectAnswer);
+           // }
+           // }
         }
 
         #endregion
