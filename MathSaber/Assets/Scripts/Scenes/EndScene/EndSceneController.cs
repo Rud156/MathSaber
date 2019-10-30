7f43a0e466f451af36e06eb03742b8f48a57a339
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-10-30 16:19:46 -0600

    Merge Master

---
+++ MathSaber/Assets/Scripts/Scenes/EndScene/EndSceneController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects.Scenes.EndScene
+{
+    public class EndSceneController : MonoBehaviour
+    {
+        #region Singleton
+
+        private static EndSceneController _instance;
+
+        public static EndSceneController Instance => _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+            }
+
+            if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        #endregion
+    }
+}
